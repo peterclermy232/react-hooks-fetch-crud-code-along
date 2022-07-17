b145f70538c54f444f3aa1af4122752943ed8edf
peterclermy <peterclermy232@gmail.com>
2022-07-17 23:25:59 +0300

    fetching data

--- .history/src/components/ItemForm_20220717205347.js
+++ .history/src/components/ItemForm_20220717205347.js
@@ -1,29 +1,11 @@
 import React, { useState } from "react";
 
-function ItemForm({ onAddItem }) {
+function ItemForm() {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    const itemData = {
-      name: name,
-      category: category,
-      isInCart: false,
-    };
-    fetch("http://localhost:4000/items", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(itemData),
-    })
-      .then((r) => r.json())
-      .then((newItem) => onAddItem(newItem));
-  }
-
   return (
-    <form className="NewItem" onSubmit={handleSubmit}>
+    <form className="NewItem">
       <label>
         Name:
         <input
---
+++ .history/src/components/Item_20220717205913.js
@@ -0,0 +1,16 @@
+i
+
+f
+ 
+ nCart ? "in-cart" : ""}>
+ an>
+ gory">{item.category}</span>
+ em.isInCart ? "remove" : "add"}>
+ emove From" : "Add to"} Cart
+ 
+ move">Delete</button>
+ 
+ 
+
+
+e
